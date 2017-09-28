9158158b56ae47d1e40da3568312171623cfdd94
Shivam Dwivedi <shivamd20@gmail.com>
2017-09-29 03:29:39 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,20 @@
+
+const express = require('express');
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const port = process.env.PORT || 3000;
+
+app.use(express.static(__dirname + '/public'));
+
+function onConnection(socket){
+  socket.on('drawing', (data) => socket.broadcast.emit('drawing', data));
+  socket.on('fdrawing', (data) => socket.broadcast.emit('fdrawing', data));
+
+  
+  
+}
+
+io.on('connection', onConnection);
+
+http.listen(port, () => console.log('listening on port ' + port));
